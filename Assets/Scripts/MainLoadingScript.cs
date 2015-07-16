cb57e0493ffed78ab486a403c5b04ed7ef30fb73
Ruben Tarin <kabum42@gmail.com>
2015-07-16 21:31:32 +0200

    Start testing with Google Play

--- Assets/Scripts/MainLoadingScript.cs
+++ Assets/Scripts/MainLoadingScript.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using GooglePlayGames.BasicApi.SavedGame;
+using System;
 
 public class MainLoadingScript : MonoBehaviour {
 
 	private int loadProgress = 0;
 	private AsyncOperation async;
 	public GameObject progress;
+	private GameObject scene1;
 
 	// Use this for initialization
 	void Start () {
+
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+		// enables saving game progress.
+		.EnableSavedGames()
+		// registers a callback to handle game invitations received while the game is not running.
+		//.WithInvitationDelegate(<callback method>)
+		// registers a callback for turn based match notifications received while the
+		// game is not running.
+		//.WithMatchDelegate(<callback method>)
+		.Build();
+		
+		PlayGamesPlatform.InitializeInstance(config);
+		// recommended for debugging:
+		PlayGamesPlatform.DebugLogEnabled = true;
+		// Activate the Google Play Games platform
+		PlayGamesPlatform.Activate();
+
+		// authenticate user:
+		Social.localUser.Authenticate((bool success) => {
+			// handle success or failure
+		});
+
 
 		if (!GlobalData.started) { GlobalData.Start(); }
 
@@ -19,7 +47,8 @@ public class MainLoadingScript : MonoBehaviour {
 
 	IEnumerator LoadScreen() {
 
-		async = Application.LoadLevelAsync ("Game");
+		//async = Application.LoadLevelAsync ("Game");
+		async = Application.LoadLevelAdditiveAsync ("Game");
 
 		while (!async.isDone) {
 
@@ -30,8 +59,71 @@ public class MainLoadingScript : MonoBehaviour {
 
 		}
 
+		scene1 = GameObject.Find ("Scene1");
+		scene1.SetActive (false);
+		loadProgress = 100;
+
+		progress.GetComponent<TextMesh>().text = loadProgress + "%";
+
+	}
+
+	void Update () {
+
+		if ((Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.OSXPlayer || Application.platform == RuntimePlatform.OSXDashboardPlayer
+			|| Social.localUser.authenticated) && loadProgress == 100) {
+
+			scene1.SetActive (true);
+			GameObject.Find ("Scene0").SetActive (false);
+
+		}
 
 
+	}
+
+	void OpenSavedGame(string filename) {
+		ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+		savedGameClient.OpenWithAutomaticConflictResolution(filename, DataSource.ReadCacheOrNetwork,
+		                                                    ConflictResolutionStrategy.UseLongestPlaytime, OnSavedGameOpened);
+	}
+
+	public void OnSavedGameOpened(SavedGameRequestStatus status, ISavedGameMetadata game) {
+		if (status == SavedGameRequestStatus.Success) {
+			// handle reading or writing of saved game.
+		} else {
+			// handle error
+		}
+	}
+
+	void SaveGame (ISavedGameMetadata game, byte[] savedData, TimeSpan totalPlaytime) {
+		ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+		
+		SavedGameMetadataUpdate.Builder builder = new SavedGameMetadataUpdate.Builder();
+		builder = builder
+			.WithUpdatedPlayedTime(totalPlaytime)
+				.WithUpdatedDescription("Saved game at " + DateTime.Now.ToString());
+		SavedGameMetadataUpdate updatedMetadata = builder.Build();
+		savedGameClient.CommitUpdate(game, updatedMetadata, savedData, OnSavedGameWritten);
+	}
+	
+	public void OnSavedGameWritten (SavedGameRequestStatus status, ISavedGameMetadata game) {
+		if (status == SavedGameRequestStatus.Success) {
+			// handle reading or writing of saved game.
+		} else {
+			// handle error
+		}
+	}
+
+	void LoadGameData (ISavedGameMetadata game) {
+		ISavedGameClient savedGameClient = PlayGamesPlatform.Instance.SavedGame;
+		savedGameClient.ReadBinaryData(game, OnSavedGameDataRead);
+	}
+	
+	public void OnSavedGameDataRead (SavedGameRequestStatus status, byte[] data) {
+		if (status == SavedGameRequestStatus.Success) {
+			// handle processing the byte array data
+		} else {
+			// handle error
+		}
 	}
 
 
