e96ff5d644af0a4d3f76232669d76c728df5c343
Ruben Tarin <kabum42@gmail.com>
2015-08-11 01:27:59 +0200

    Revert "Ads working"
    This reverts commit 50921d4f50a35e5a9a69fdc0c2674b68bd1e0958.

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using GoogleMobileAds.Api;
 
 public class MainScript : MonoBehaviour {
 
@@ -26,9 +25,6 @@ public class MainScript : MonoBehaviour {
 
 	private float slideToLeft = 0f;
 	private float slideToRight = 0f;
-
-	private InterstitialAd interstitial;
-	private bool showedAd = false;
 
 	// Use this for initialization
 	void Start () 
@@ -89,23 +85,10 @@ public class MainScript : MonoBehaviour {
 
 		auxScreen = camera.rect;
 
-		interstitial = new InterstitialAd("ca-app-pub-7511456809904271/6234934949");
-		// Create an empty ad request.
-		AdRequest request = new AdRequest.Builder().Build();
-		// Load the interstitial with the request.
-		interstitial.LoadAd(request);
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (!showedAd) {
-			if (interstitial.IsLoaded()) {
-				showedAd = true;
-				interstitial.Show();
-			}
-		}
 
 		GlobalData.Update ();
 		capa2TopText.GetComponent<TextMesh> ().text = GlobalData.FormattedNumber(GlobalData.thisState.love);
