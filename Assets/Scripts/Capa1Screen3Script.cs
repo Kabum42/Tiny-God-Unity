0721a0801842485b934e4c2656eca62d8fe27992
Ruben Tarin <kabum42@gmail.com>
2015-08-15 19:59:06 +0200

    Producers improving 4

--- Assets/Scripts/Capa1Screen3Script.cs
+++ Assets/Scripts/Capa1Screen3Script.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Capa1Screen3Script : MonoBehaviour {
 
+	private Producer[]producers = new Producer[9];
+
 	private Producer servant;
 	private Producer human;
 	private Producer prophet;
@@ -12,8 +14,6 @@ public class Capa1Screen3Script : MonoBehaviour {
 	private Producer laboratory;
 	private Producer shop;
 	private Producer spaceship;
-
-
 
 	private float discovered_transparency = 0.75f;
 
@@ -54,101 +54,122 @@ public class Capa1Screen3Script : MonoBehaviour {
 		shop = new Producer (this.gameObject, 7, "Shop", Lang.SHOP_NAME);
 		spaceship = new Producer (this.gameObject, 8, "Spaceship", Lang.SPACESHIP_NAME);
 
-		/*
-		servant = GameObject.Find ("Scene1/Capa1/Screen3/Servant");
-		servantBG = GameObject.Find ("Scene1/Capa1/Screen3/Servant/Background");
-		servantIcon = GameObject.Find ("Scene1/Capa1/Screen3/Servant/Icon");
-		servantText = GameObject.Find ("Scene1/Capa1/Screen3/Servant/Text");
-		servantNumber = GameObject.Find ("Scene1/Capa1/Screen3/Servant/Number");
-		servantSubNumber = GameObject.Find ("Scene1/Capa1/Screen3/Servant/SubNumber");
-		servantCost = GameObject.Find ("Scene1/Capa1/Screen3/Servant/Cost");
-		servantLoveIcon = GameObject.Find ("Scene1/Capa1/Screen3/Servant/LoveIcon");
-		servantBG.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, discovered_transparency);
-		servantIcon.GetComponent<SpriteRenderer>().material.color = new Color(0f, 0f, 0f, discovered_transparency);
-		servantText.GetComponent<TextMesh> ().color = new Color(servantText.GetComponent<TextMesh> ().color.r, servantText.GetComponent<TextMesh> ().color.g, servantText.GetComponent<TextMesh> ().color.b, discovered_transparency);
-		servantNumber.GetComponent<TextMesh> ().color = new Color(servantNumber.GetComponent<TextMesh> ().color.r, servantNumber.GetComponent<TextMesh> ().color.g, servantNumber.GetComponent<TextMesh> ().color.b, discovered_transparency);
-		servantSubNumber.GetComponent<TextMesh> ().color = new Color(servantSubNumber.GetComponent<TextMesh> ().color.r, servantSubNumber.GetComponent<TextMesh> ().color.g, servantSubNumber.GetComponent<TextMesh> ().color.b, discovered_transparency);
-		servantCost.GetComponent<TextMesh> ().color = new Color(servantCost.GetComponent<TextMesh> ().color.r, servantCost.GetComponent<TextMesh> ().color.g, servantCost.GetComponent<TextMesh> ().color.b, discovered_transparency);
-		servantNumber.SetActive (false);
-		servantSubNumber.SetActive (false);
-		servantCost.SetActive (false);
-		servantLoveIcon.SetActive (false);
-		*/
+		producers [0] = servant;
+		producers [1] = human;
+		producers [2] = prophet;
+		producers [3] = temple;
+		producers [4] = ship;
+		producers [5] = factory;
+		producers [6] = laboratory;
+		producers [7] = shop;
+		producers [8] = spaceship;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		// THIS REGULAR COMPROBATION IS SPECIAL, HAS NO PREVIOUS PRODUCER
 		RegularComprobation ("meh", ref servant);
-		RegularComprobation (servant.status, ref human);
-		RegularComprobation (human.status, ref prophet);
-		RegularComprobation (prophet.status, ref temple);
-		RegularComprobation (temple.status, ref ship);
-		RegularComprobation (ship.status, ref factory);
-		RegularComprobation (factory.status, ref laboratory);
-		RegularComprobation (laboratory.status, ref shop);
-		RegularComprobation (shop.status, ref spaceship);
 
-		if (servant.status != "unexistant" && servant.status != "undiscovered" && ClickedOn (servant.buyButton)) {	
-			checkCanBuy (servant);
-		} else if (false) {
-			// COMPROBAR SI HA CLICKADO ENCIMA DE LA CABECERA
+		// OTHER REGULAR COMPROBATIONS
+		for (int i = 1; i < producers.Length; i++) {
+			RegularComprobation (producers[i-1].status, ref producers[i]);
 		}
 
-		if (human.status != "unexistant" && human.status != "undiscovered" && ClickedOn (human.buyButton)) {	
-			checkCanBuy (human);
-		}
+		// CHECK FOR BUYINGS
+		for (int i = 0; i < producers.Length; i++) {
 
-		if (prophet.status != "unexistant" && prophet.status != "undiscovered" && ClickedOn (prophet.buyButton)) {	
-			checkCanBuy (prophet);
-		}
+			if (producers[i].status != "unexistant" && producers[i].status != "undiscovered" && ClickedOn (producers[i].buyButton)) {	
+				if (checkCanBuy(producers[i])) {
+					buy (producers[i]);
+				}
+			} else if (false) {
+				// COMPROBAR SI HA CLICKADO ENCIMA DE LA CABECERA
+			}
 
-		if (temple.status != "unexistant" && temple.status != "undiscovered" && ClickedOn (temple.buyButton)) {	
-			checkCanBuy (temple);
-		}
-
-		if (ship.status != "unexistant" && ship.status != "undiscovered" && ClickedOn (ship.buyButton)) {	
-			checkCanBuy (ship);
-		}
-
-		if (factory.status != "unexistant" && factory.status != "undiscovered" && ClickedOn (factory.buyButton)) {	
-			checkCanBuy (factory);
-		}
-
-		if (laboratory.status != "unexistant" && laboratory.status != "undiscovered" && ClickedOn (laboratory.buyButton)) {	
-			checkCanBuy (laboratory);
-		}
-
-		if (shop.status != "unexistant" && shop.status != "undiscovered" && ClickedOn (shop.buyButton)) {	
-			checkCanBuy (shop);
-		}
-
-		if (spaceship.status != "unexistant" && spaceship.status != "undiscovered" && ClickedOn (spaceship.buyButton)) {	
-			checkCanBuy (spaceship);
 		}
 
 
 	}
 
 	private void RegularComprobation(string previousStatus, ref Producer producer) {
-		
+
+		// UNEXISTANT TO UNDISCOVERED
 		if (producer.status == "unexistant" && (previousStatus != "undiscovered" && previousStatus != "unexistant")) {
 			producer.root.SetActive(true);
 			producer.status = "undiscovered";
-		} else if ((producer.status == "undiscovered" && GlobalData.thisState.totalLove >= GlobalData.getBaseCost (producer.langCode)) ||
+		} 
+		// UNDISCOVERED/BUYABLE TO DISCOVERED
+		else if ((producer.status == "undiscovered" && GlobalData.thisState.totalLove >= GlobalData.getBaseCost (producer.langCode)) ||
 		           (producer.status == "buyable" && GlobalData.thisState.love < Mathf.Floor(GlobalData.getBaseCost (producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode])))) {
 			producer.status = "discovered";
-		} else if (producer.status == "discovered" && GlobalData.thisState.love >= Mathf.Floor (GlobalData.getBaseCost (producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode]))) {
+		} 
+		// DISCOVERED TO BUYABLE
+		else if (producer.status == "discovered" && GlobalData.thisState.love >= Mathf.Floor (GlobalData.getBaseCost (producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode]))) {
 			producer.status = "buyable";
 		}
-		
+
+		// UPDATE TEXT (LANGUAGE), NUMBER AND COST
 		if (producer.status != "unexistant" && producer.status != "undiscovered") {
 			producer.text.GetComponent<TextMesh> ().text = Lang.getText(producer.langCode);
 			producer.number.GetComponent<TextMesh> ().text = ""+GlobalData.thisState.values[producer.langCode];
 			producer.cost.GetComponent<TextMesh> ().text = ""+GlobalData.FormattedNumber(Mathf.Floor (GlobalData.getBaseCost (producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode])));
 		}
-		
+
+		// ANIMATIONS: LOCKED TO UNLOCKING
+		if (!producer.number.activeInHierarchy && (producer.status != "unexistant" && producer.status != "undiscovered")) {
+
+			producer.number.SetActive(true);
+			producer.cost.SetActive(true);
+			producer.loveIcon.SetActive(true);
+			
+			producer.buyButton.GetComponent<Animator> ().Play ("Unlocking", 0, 0f);
+			producer.icon.GetComponent<Animator> ().Play ("Unlocking", 0, 0f);
+			producer.screen.GetComponent<Animator> ().Play ("Unlocking", 0, 0f);
+
+		}
+
+		// ANIMATIONS: UNLOCKING TO AVAILABLE
+		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Unlocking") && producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).normalizedTime >= 1f) {
+			producer.buyButton.GetComponent<Animator> ().Play ("Available", 0, 0f);
+			producer.icon.GetComponent<Animator> ().Play ("Available", 0, 0f);
+			producer.screen.GetComponent<Animator> ().Play ("Available", 0, 0f);
+		}
+
+		// ANIMATIONS: BUYING TO AVAILABLE
+		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Buying") && producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).normalizedTime >= 1f) {
+			producer.buyButton.GetComponent<Animator> ().Play ("Available", 0, 0f);
+			producer.icon.GetComponent<Animator> ().Play ("Available", 0, 0f);
+		}
+
+		// ANIMATIONS: AVAILABLE TO CHANGING (NOT AVAILABLE)
+		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Available") && !checkCanBuy(producer)) {
+			producer.buyButton.GetComponent<Animator> ().Play ("Changing", 0, 0f);
+			producer.screen.GetComponent<Animator> ().Play ("Changing", 0, 0f);
+		}
+
+		// ANIMATIONS: CHANGING (NOT AVAILABLE) TO UNAVAILABLE
+		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Changing") && producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).normalizedTime >= 1f) {
+			producer.buyButton.GetComponent<Animator> ().Play ("Unavailable", 0, 0f);
+			producer.screen.GetComponent<Animator> ().Play ("Unavailable", 0, 0f);
+		}
+
+		// ANIMATIONS: UNAVAILABLE TO CHANGING2 (AVAILABLE)
+		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Unavailable") && checkCanBuy(producer)) {
+			producer.buyButton.GetComponent<Animator> ().Play ("Changing2", 0, 0f);
+			producer.screen.GetComponent<Animator> ().Play ("Changing2", 0, 0f);
+		}
+
+		// ANIMATIONS: CHANGING2 (AVAILABLE) TO AVAILABLE
+		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Changing2") && producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).normalizedTime >= 1f) {
+			producer.buyButton.GetComponent<Animator> ().Play ("Available", 0, 0f);
+			producer.screen.GetComponent<Animator> ().Play ("Available", 0, 0f);
+		}
+
+
+		// CHANGE TRANSPARENCIES
+		/*
 		float producerAlpha = 1f;
 		
 		if (selectedStatus == 0f) {
@@ -157,62 +178,53 @@ public class Capa1Screen3Script : MonoBehaviour {
 
 
 		if (producer.status == "undiscovered") {
-			//background.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, discovered_transparency*producerAlpha);
 			//icon.GetComponent<SpriteRenderer>().material.color = new Color(0f, 0f, 0f, discovered_transparency*producerAlpha);
 			producer.text.GetComponent<TextMesh> ().color = new Color(producer.text.GetComponent<TextMesh> ().color.r, producer.text.GetComponent<TextMesh> ().color.g, producer.text.GetComponent<TextMesh> ().color.b, discovered_transparency*producerAlpha);
 			producer.number.GetComponent<TextMesh> ().color = new Color(producer.number.GetComponent<TextMesh> ().color.r, producer.number.GetComponent<TextMesh> ().color.g, producer.number.GetComponent<TextMesh> ().color.b, discovered_transparency*producerAlpha);
 		} else if (producer.status == "discovered") {
-			//background.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, discovered_transparency*producerAlpha);
 			//icon.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, discovered_transparency*producerAlpha);
 			producer.text.GetComponent<TextMesh> ().color = new Color(producer.text.GetComponent<TextMesh> ().color.r, producer.text.GetComponent<TextMesh> ().color.g, producer.text.GetComponent<TextMesh> ().color.b, discovered_transparency*producerAlpha);
 			producer.number.GetComponent<TextMesh> ().color = new Color(producer.number.GetComponent<TextMesh> ().color.r, producer.number.GetComponent<TextMesh> ().color.g, producer.number.GetComponent<TextMesh> ().color.b, discovered_transparency*producerAlpha);
 		} else if (producer.status == "buyable") {
-			//background.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 1f*producerAlpha);
 			//icon.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, 1f*producerAlpha);
 			producer.text.GetComponent<TextMesh> ().color = new Color(producer.text.GetComponent<TextMesh> ().color.r, producer.text.GetComponent<TextMesh> ().color.g, producer.text.GetComponent<TextMesh> ().color.b, 1f*producerAlpha);
 			producer.number.GetComponent<TextMesh> ().color = new Color(producer.number.GetComponent<TextMesh> ().color.r, producer.number.GetComponent<TextMesh> ().color.g, producer.number.GetComponent<TextMesh> ().color.b, 1f*producerAlpha);
 		}
-		
-		if (!producer.number.activeInHierarchy && (producer.status != "unexistant" && producer.status != "undiscovered")) {
-			producer.number.SetActive(true);
-			//subNumber.SetActive(true);
-			producer.cost.SetActive(true);
-			producer.loveIcon.SetActive(true);
-
-			producer.buyButton.GetComponent<Animator> ().Play ("Unlocking", 0, 0f);
-			producer.icon.GetComponent<Animator> ().Play ("Unlocking", 0, 0f);
-			producer.screen.GetComponent<Animator> ().Play ("Unlocking", 0, 0f);
-		}
-
-		if (producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).IsName ("Unlocking") && producer.buyButton.GetComponent<Animator> ().GetCurrentAnimatorStateInfo (0).normalizedTime >= 1f) {
-			producer.buyButton.GetComponent<Animator> ().Play ("Available", 0, 0f);
-			producer.screen.GetComponent<Animator> ().Play ("Available", 0, 0f);
-		}
-
+		*/
 		
 	}
 
-	private void checkCanBuy(Producer producer) {
+	private bool checkCanBuy(Producer producer) {
 
-		if (GlobalData.thisState.love >= Mathf.Floor (GlobalData.getBaseCost(producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode]))) {
+		if (GlobalData.thisState.love >= Mathf.Floor (GlobalData.getBaseCost (producer.langCode) * Mathf.Pow (1.1f, GlobalData.thisState.values [producer.langCode]))) {
 			
-			GlobalData.thisState.love -= Mathf.Floor (GlobalData.getBaseCost(producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode]));
-			GlobalData.thisState.values[producer.langCode]++;
+			return true;
 			
-			float aux = Random.Range(0f, 1f);
+		} else {
 
-			producer.buyButton.GetComponent<Animator> ().Play ("Buying", 0, 0f);
-			producer.icon.GetComponent<Animator> ().Play ("Buying", 0, 0f);
-			
-			if (aux > 2f/3f) {
-				buy1.Play();
-			} else if (aux > 1f/3f) {
-				buy2.Play();
-			}
-			else {
-				buy3.Play();
-			}
-			
+			return false;
+
+		}
+
+	}
+
+	private void buy (Producer producer) {
+
+		GlobalData.thisState.love -= Mathf.Floor (GlobalData.getBaseCost(producer.langCode)*Mathf.Pow(1.1f, GlobalData.thisState.values[producer.langCode]));
+		GlobalData.thisState.values[producer.langCode]++;
+		
+		float aux = Random.Range(0f, 1f);
+		
+		producer.buyButton.GetComponent<Animator> ().Play ("Buying", 0, 0f);
+		producer.icon.GetComponent<Animator> ().Play ("Buying", 0, 0f);
+		
+		if (aux > 2f/3f) {
+			buy1.Play();
+		} else if (aux > 1f/3f) {
+			buy2.Play();
+		}
+		else {
+			buy3.Play();
 		}
 
 	}
@@ -298,6 +310,8 @@ public class Capa1Screen3Script : MonoBehaviour {
 		public GameObject buyButton;
 		public GameObject icon;
 		public GameObject screen;
+		public GameObject hb_head;
+
 
 		public Producer(GameObject parent, int position, string name, int langAux) {
 
@@ -318,6 +332,7 @@ public class Capa1Screen3Script : MonoBehaviour {
 			buyButton = root.gameObject.transform.FindChild("BuyButton").gameObject;
 			icon = root.gameObject.transform.FindChild("Icon").gameObject;
 			screen = root.gameObject.transform.FindChild("Screen").gameObject;
+			hb_head = root.gameObject.transform.FindChild("hb_head").gameObject;
 
 			root.SetActive(false);
 
